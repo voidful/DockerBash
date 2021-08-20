2a35e4d8ed693a9bdbf3d9627bf5909246843d86
Crazycurly <sam60317@gmail.com>
2021-08-20 10:13:24 +0800

    Update create_docker.sh

--- create_docker.sh
+++ create_docker.sh
@@ -6,25 +6,42 @@ if [ ${#USER_PWD} == 0 ] ; then
     USER_PWD="password"
 fi
 
-read -p "Setting SSH's Port  : " USER_PORT
+read -p "Setting SSH's Port  : " SSH_PORT
+read -p "Setting published ports (8080:8080 5000:5000...)  : " USER_PORT
+read -p "Docker image  : " IMAGE
+read -p "Apt package (nano vim...) : " PACKAGE
 
-if [ ${#USER_NAME} == 0 -o ${#USER_PORT} == 0 ] ; then
+
+if [ ${#USER_NAME} == 0 -o ${#SSH_PORT} == 0 -o ${#USER_PORT} == 0] ; then
     echo "Please at least enter the user name and the port number"
     exit 0
 fi
 
+arrPORT=(${USER_PORT// / })
+
 echo -e "\nYou Setting:"
 echo "User: ${USER_NAME}"
 echo "Password: ${USER_PWD}"
-echo "Port: ${USER_PORT}"
+echo "SSH Port: ${SSH_PORT}"
+echo "Published ports: "
+
+len=${#arrPORT[@]}
+
+for (( i=0; i<${len}; i++ ));
+do
+  echo "${arrPORT[$i]}"
+  Port+="-p ${arrPORT[$i]} "
+done
+
 
 sudo docker run -itd \
-                -p $USER_PORT:22 \
+                -p $SSH_PORT:22 \
+                ${Port} \
                 --name $USER_NAME \
                 --hostname $USER_NAME \
-                ubuntu:20.04
+                ${IMAGE}
 
-sudo docker exec -ti $USER_NAME sh -c "apt-get update && apt-get -y upgrade && apt-get install -y openssh-server micro nano"
+sudo docker exec -ti $USER_NAME sh -c "apt-get update && apt-get -y upgrade && apt-get install -y openssh-server ${PACKAGE}"
 
 
 sudo docker exec -ti $USER_NAME sh -c "echo \"root:${USER_PWD}\" | chpasswd;
@@ -39,4 +56,4 @@ sudo docker restart $USER_NAME
 sudo docker exec -ti $USER_NAME sh -c "service ssh start"
 
 echo "Container create finish"
-
+echo "Login as root, SSH port ${SSH_PORT}"
